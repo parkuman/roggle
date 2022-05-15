6c9c61815de0491615345e851c66ce21ff5496c5
Parker Rowe <parker.rowe@ericsson.com>
2022-05-15 14:02:03 -0400

    initial working Trie

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use roggle;
+use roggle::Trie;
 
 fn main() {
-  let board = "1 2 3 4";
-  println!("Board: {}", board);
-  println!("Solved: {}", roggle::solve(&board));
+  // let board = "1 2 3 4";
+  // println!("Board: {}", board);
+  // println!("Solved: {}", roggle::solve(&board));
+  let mut trie = Trie::new();
+  trie.insert("hello");
+  trie.insert("donald");
+  println!("{}", trie.find("hello"));
+  println!("{}", trie.find("hell"));
+  println!("{}", trie.find("h"));
+  println!("{}", trie.find("donald"));
+  println!("{}", trie.find("donal"));
+  println!("{}", trie.find("parker"));
+  println!("{}", trie.find("hellothere"));
 }
