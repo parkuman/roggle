5db3da21a325279f8ac1a62bdbffee0741b7c8d1
Parker Rowe <parkumania@gmail.com>
2022-05-18 01:52:09 -0400

    add board validation

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod board;
 mod trie;
 
 use board::Board;
+use std::process::exit;
 use trie::Trie;
 
 use std::{
@@ -43,8 +44,45 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
   }
 }
 
-pub fn solve(board_string: &str) -> Vec<String> {
-  // let word_list = lines_from_file("word_lists/words_alpha.txt");
+fn validate_board(board_input: &str) -> &str {
+  if board_input.is_empty() {
+    println!("Empty board provided!");
+    exit(1);
+  }
+
+  let board_rows: Vec<&str> = board_input.split(" ").collect();
+  let mut max_row_len: Option<usize> = None; // a running tracker to make sure all rows are the same length
+
+  for row in board_rows {
+    let row_len = row.chars().collect::<Vec<char>>().len();
+
+    match max_row_len {
+      None => {
+        // if max_row_len doesn't have a value, set it to the current row length.
+        max_row_len = Some(row_len);
+      }
+      Some(i) => {
+        // if max_row_len has a value, ensure that this new row we're checking is the same size as it. if it is, set max_row_len and proceed
+        if i != row_len {
+          println!(
+            "rows must be of equal size! row \"{}\" is not the same length as the rest",
+            row
+          );
+          exit(1);
+        } else {
+          max_row_len = Some(i);
+        }
+      }
+    }
+  }
+
+  board_input
+}
+
+pub fn solve(input_board: &str) -> Vec<String> {
+  let board_string = input_board.trim();
+  validate_board(board_string);
+
   let word_list = include_str!("../word_lists/words_alpha.txt")
     .split("\r\n")
     .map(|str| str.to_string())
--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,6 @@ use roggle;
 use std::env::args;
 use std::process::exit;
 
-fn validate_board(board_input: &str) -> &str {
-  let board_rows: Vec<&str> = board_input.split(" ").collect();
-  let mut max_row_len: Option<usize> = None; // a running tracker to make sure all rows are the same length
-
-  for row in board_rows {
-    let row_len = row.chars().collect::<Vec<char>>().len();
-
-    match max_row_len {
-      None => {
-        // if max_row_len doesn't have a value, set it to the current row length.
-        max_row_len = Some(row_len);
-      }
-      Some(i) => {
-        // if max_row_len has a value, ensure that this new row we're checking is the same size as it. if it is, set max_row_len and proceed
-        if i != row_len {
-          println!(
-            "rows must be of equal size! row \"{}\" is not the same length as the rest",
-            row
-          );
-          exit(1);
-        } else {
-          max_row_len = Some(i);
-        }
-      }
-    }
-  }
-
-  board_input
-}
-
 fn main() {
   let args: Vec<String> = args().collect();
 
@@ -40,16 +10,10 @@ fn main() {
       println!("No board argument provided!");
       exit(1);
     }
-    2 => {
-      if args[1].is_empty() {
-        println!("No board argument provided!");
-        exit(1);
-      }
-    }
     _ => (),
   }
 
-  let board = validate_board(&args[1].trim());
+  let board = &args[1];
 
   println!("solutions: {:?}", roggle::solve(board));
 }
