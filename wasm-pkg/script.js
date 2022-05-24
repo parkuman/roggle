692a3699c3cbc14ccf4da7b2eef314cc0b28bc29
Parker Rowe <parkumania@gmail.com>
2022-05-23 21:48:27 -0400

    fix tag

--- wasm-pkg/script.js
+++ wasm-pkg/script.js
@@ -1,10 +1,12 @@
 import init, { solve } from "./pkg/roggle.js";
 
 function solve_board() {
-  const board = "qeen zzzz";
-
+  const board = "efkj keps aoeq";
+  const start = new Date();
   console.log("Solving...");
   console.log(solve(board));
+  const end = new Date();
+  console.log("completed in", (end - start) / 1000, "seconds");
 }
 
 init().then(solve_board);
